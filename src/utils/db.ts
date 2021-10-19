ced76d54cf1f94db56a25c44b2452ede7912bccc
DiogoReiss <diogommreis1@gmail.com>
2021-10-19 23:15:59 +0100

    fix: removing postgres connection string

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 
 config()
 
-const connectionString = process.env.CONNECTION_STRING || 'postgres://dsnbkkcb:r5fx9nWtni0NLWrqEL1d9igYX6cz5IOv@tai.db.elephantsql.com/dsnbkkcb'
+const connectionString = process.env.CONNECTION_STRING
 const db = new Pool({ connectionString })
 
 export default db
